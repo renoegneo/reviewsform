44f1e9f7b518a1d4874f4e72683be8ad7b3f7846
Maxut <micmac337@mail.ru>
2025-12-07 00:03:04 +0600

    ебучие слеши

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -5,7 +5,7 @@
  * Для продакшена: просто замени на URL твоего хостинга
  */
 
-const API_URL = 'https://reviewsform-production.up.railway.app/';
+const API_URL = 'https://reviewsform-production.up.railway.app';
 
 // Эндпоинты API
 const API_ENDPOINTS = {
